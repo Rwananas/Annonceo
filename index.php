23e41b9a95ebf925d21aef249b0069bb56bd3813
erwannbruneteau <erwannthomas@gmail.com>
2023-03-12 13:22:23 +0100

    add pagination pour page index

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 require_once('include/init.php');
 
-// code a venir
+// PAGINATION GESTION ANNONCES
+
+
+// Si un indice page existe dans l'URL et qu'on trouve une valeur dedans 
+if (isset($_GET['page']) && !empty($_GET['page'])) {
+    $pageCourante = (int) strip_tags($_GET['page']);
+} else {
+    $pageCourante = 1;
+}
+$queryAnnonces = $pdo->query(" SELECT COUNT(id_annonce) AS nombreAnnonces FROM Annonce ");
+$resultatAnnonces = $queryAnnonces->fetch();
+$nombreAnnonces = (int) $resultatAnnonces['nombreAnnonces'];
+$parPage = 6;
+$nombresPages = ceil($nombreAnnonces / $parPage);
+$premierAnnonce = ($pageCourante - 1) * $parPage;
+
+// FIN PAGINATION GESTION ANNONCES
 
 require_once('include/affichage.php');
 require_once('include/header.php');
@@ -53,27 +69,34 @@ require_once('include/header.php');
                 <?php endwhile; ?>
             </div>
 
-            <nav aria-label="">
-                <ul class="pagination justify-content-end">
-                    <li class="mx-1 page-item  ">
-                        <a class="page-link text-info" href="" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </li>
-                    <!--  -->
-                    <li class="mx-1 page-item ">
-                        <a class="btn btn-outline-info " href=""></a>
-                    </li>
-                    <!--  -->
-                    <li class="mx-1 page-item ">
-                        <a class="page-link text-info" href="" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+            <nav>
+    <ul class="pagination justify-content-end">
+        <!-- Dans le cas ou nous sommes sur la page 1, ternaire pour ajouter la class Bootstrap 'disabled' afin de désactiver le bouton précédent car il n'y a pas de page précédente à la 1ere  -->
+        <li class="page-item <?= ($pageCourante == 1) ? 'disabled': "" ?>">
+        <!-- Si on clique sur la flêche précédente, c'est pour aller à la page précédent, dans ce cas on soustrait à $pageCourante, la valeur de 1 (si pageCourante = 4, on retournera à la page 3) -->
+            <a class="page-link text-dark" href="?page=<?= $pageCourante - 1 ?>" aria-label="Previous">
+                <span aria-hidden="true">précédente</span>
+                <span class="sr-only">Previous</span>
+            </a>
+        </li>
+        
+        <!-- AFFICHAGE DU NOMBRE DE PAGE -->
+        <?php for($page = 1; $page <= $nombresPages; $page++): ?>
+        <li class="mx-1 page-item ">
+            <a class="btn btn-outline-dark <?= ($pageCourante == $page) ? 'active': "" ?>" href="?page=<?= $page?>"><?= $page?></a>
+        </li>
+        <?php endfor; ?>
+        <!-- Fin d'affichage nombre de pages -->
+
+
+        <li class="page-item <?= ($pageCourante == $nombresPages) ? 'disabled': "" ?>">
+            <a class="page-link text-dark" href="?page=<?= $pageCourante + 1 ?>" aria-label="Next">
+                <span aria-hidden="true">suivante</span>
+                <span class="sr-only">Next</span>
+            </a>
+        </li>
+    </ul>
+</nav>
 
         </div>
 
