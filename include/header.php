038431a620dd7f7bad6f4c0f1228984f844078ea
erwannbruneteau <erwannthomas@gmail.com>
2023-03-12 13:27:47 +0100

    link page QSN via le header

--- include/header.php
+++ include/header.php
@@ -62,7 +62,7 @@ $afficheMenuPublics = $pdo->query(" SELECT DISTINCT titre FROM categorie ORDER B
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item mt-2 mx-2">
-        <a class="nav-link" href="<?= URL ?>">Qui Sommes Nous</a>
+        <a class="nav-link" href="<?= URL ?>QSN.php">Qui Sommes Nous</a>
       </li>
       <li class="nav-item mt-2">
         <a class="nav-link" href="<?= URL ?>">Contact</a>
