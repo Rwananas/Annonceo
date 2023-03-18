c44145a8fe04763e9f471dd31aed47d67b363d85
erwannbruneteau <erwannthomas@gmail.com>
2023-03-18 01:56:35 +0100

    modif header + creation form contact

--- contact.php
+++ contact.php
@@ -5,6 +5,25 @@ require_once('include/header.php');
 ?>
 
 
+<form action="#" method="POST">
+  <div class="form-group">
+    <label for="name">Nom complet</label>
+    <input type="text" class="form-control" id="name" name="name" required>
+  </div>
+  <div class="form-group">
+    <label for="email">Adresse e-mail</label>
+    <input type="email" class="form-control" id="email" name="email" required>
+  </div>
+  <div class="form-group">
+    <label for="subject">Sujet</label>
+    <input type="text" class="form-control" id="subject" name="subject" required>
+  </div>
+  <div class="form-group">
+    <label for="message">Message</label>
+    <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Envoyer</button>
+</form>
 
 
 
--- include/header.php
+++ include/header.php
@@ -65,14 +65,14 @@ $afficheMenuPublics = $pdo->query(" SELECT DISTINCT titre FROM categorie ORDER B
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
+      <li class="nav-item mt-2">
+        <a class="nav-link" href="<?= URL ?>index.php">Accueil</a>
+      </li>
       <li class="nav-item mt-2 mx-2">
         <a class="nav-link" href="<?= URL ?>QSN.php">Qui Sommes Nous</a>
       </li>
       <li class="nav-item mt-2">
         <a class="nav-link" href="<?= URL ?>contact.php">Contact</a>
-      </li>
-      <li class="nav-item mt-2">
-        <a class="nav-link" href="<?= URL ?>annonce.php">Annonces</a>
       </li>
       
     </ul>
