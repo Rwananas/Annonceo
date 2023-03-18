f9f242b224c3d2058ae3a6ee5c6f9353279a0c8b
erwannbruneteau <erwannthomas@gmail.com>
2023-03-18 01:32:28 +0100

    modif footer

--- include/footer.php
+++ include/footer.php
@@ -1,21 +1,18 @@
-
-
 <!-- Footer -->
 </div>
 
 <!-- ======= Footer ======= -->
 <footer id="footer">
-    <div class="container">
-      <div class="copyright">
+  <div class="container text-center">
+    <div class="copyright text-center">
         &copy; Copyright <strong><span>Annonceo</span></strong>. All Rights Reserved
       </div>
-      <div class="credits">
-        
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
       </div>
     </div>
-  </footer><!-- End #footer -->
+  </div>
+</footer><!-- End #footer -->
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <script src="/admin/vendor/bootstrap/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -24,53 +21,55 @@
 
 <!-- script pour popup de confirmation, à la place du on-click -->
 <script>
-    $(document).ready(function() {
+  $(document).ready(function() {
     $('a[data-confirm]').click(function(ev) {
-        var href = $(this).attr('href');
+      var href = $(this).attr('href');
 
-        if (!$('#dataConfirmModal').length) {
-            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
-        } 
-        $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
-        $('#dataConfirmOK').attr('href', href);
-        $('#dataConfirmModal').modal({show:true});
-        return false;
+      if (!$('#dataConfirmModal').length) {
+        $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+      }
+      $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
+      $('#dataConfirmOK').attr('href', href);
+      $('#dataConfirmModal').modal({
+        show: true
+      });
+      return false;
     });
-});
-
-$('#confirm-delete').on('show.bs.modal', function(e) {
-    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
-});
-
-/*POST
-
-// Bind click to OK button within popup
-$('#confirm-delete').on('click', '.btn-ok', function(e) {
-
-  var $modalDiv = $(e.delegateTarget);
-  var id = $(this).data('recordId');
-
-  $modalDiv.addClass('loading');
-  $.post('/api/record/' + id).then(function() {
-     $modalDiv.modal('hide').removeClass('loading');
   });
-});
 
-// Bind to modal opening to set necessary data properties to be used to make request
-$('#confirm-delete').on('show.bs.modal', function(e) {
-  var data = $(e.relatedTarget).data();
-  $('.title', this).text(data.recordTitle);
-  $('.btn-ok', this).data('recordId', data.recordId);
-});
+  $('#confirm-delete').on('show.bs.modal', function(e) {
+    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+  });
 
-//OLD
+  /*POST
+
+  // Bind click to OK button within popup
+  $('#confirm-delete').on('click', '.btn-ok', function(e) {
+
+    var $modalDiv = $(e.delegateTarget);
+    var id = $(this).data('recordId');
+
+    $modalDiv.addClass('loading');
+    $.post('/api/record/' + id).then(function() {
+       $modalDiv.modal('hide').removeClass('loading');
+    });
+  });
+
+  // Bind to modal opening to set necessary data properties to be used to make request
+  $('#confirm-delete').on('show.bs.modal', function(e) {
+    var data = $(e.relatedTarget).data();
+    $('.title', this).text(data.recordTitle);
+    $('.btn-ok', this).data('recordId', data.recordId);
+  });
+
+  //OLD
 
 
 
-*/
-
+  */
 </script>
 
-   
+
 </body>
+
 </html>
